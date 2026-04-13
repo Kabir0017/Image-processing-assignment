a81cb68c407ca06894f33a6efd8ac32f69aeeb91
Kabir0017 <kabirbidhuri22@gmail.com>
2026-04-13 21:26:19 +0530

    Update traffic_monitoring.py

--- Image_Assignment-main/Assign_4(edge_detec,ORB)/traffic_monitoring.py
+++ Image_Assignment-main/Assign_4(edge_detec,ORB)/traffic_monitoring.py
@@ -1,5 +1,5 @@
-# Name: Niti Dagar
-# Roll No: 2301010443
+# Name: Kabir Bidhuri
+# Roll No: 2301010461
 # Course: BTech CSE CORE
 # Unit 4: Edge detection, object representation, feature extraction
 # Assignment Title: Traffic Monitoring with Edge Detection and ORB
